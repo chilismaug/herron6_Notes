5c38724e8594a45826131e00c22518476f1f3887
chilismaug <charles.ross.oh@gmail.com>
2014-07-21 08:34:54 -0400

    db connect - back-diddled due to function signatures, no works w/foreman, not sure about heroku

--- notesdb-mongoose.js
+++ notesdb-mongoose.js
@@ -7,7 +7,7 @@ process.env.MONGOLAB_URI ||
 process.env.MONGOHQ_URL ||
  'mongodb://localhost/chap06';
 
-/*
+
 exports.connect = function(callback) {
     mongoose.connect(uristring);
 }
@@ -15,8 +15,8 @@ exports.connect = function(callback) {
 exports.disconnect = function(callback) {
     mongoose.disconnect(callback);
 }
-*/
 
+/*
 exports.connect = mongoose.connect(uristring, function(err, res){
     if (err){
         console.log('ERROR connecting to ' + uristring + '. ' + err);
@@ -24,7 +24,7 @@ exports.connect = mongoose.connect(uristring, function(err, res){
         console.log('Succeeded, connected to ' + uristring );
     }
 });
-
+*/
 
 
 exports.setup = function(callback) { callback(null); }
